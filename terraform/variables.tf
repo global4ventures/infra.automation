0e0a4ce317f249d3f8d83a253b921a4396c17811
global4ventures <divit4globalventures@gmail.com>
2024-11-27 10:58:04 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,44 +21,51 @@ variable "tag_email" {
 variable "tag_cost_center" {
   description = "Cost center for the resources"
   type        = string
-  default     = "0000"
+  default     = "12345"
 }
 
 # S3 Bucket Variables
 variable "s3_data_bucket_name" {
   description = "Name of the S3 bucket for RAG data"
   type        = string
+  default     = "ragdatabucket"
 }
 
 variable "s3_internal_data_bucket_name" {
   description = "Name of the S3 bucket for internal data"
   type        = string
+  default     = "internal-databucket"
 }
 
 # Lambda Variables
 variable "metadata_lambda_name" {
   description = "Name of the Metadata Extraction Lambda"
   type        = string
+  default     = "metadata-lambda"
 }
 
 variable "data_pull_lambda_name" {
   description = "Name of the Data Pull Lambda"
   type        = string
+  default     = "datapull-lambda"
 }
 
 variable "data_prepping_lambda_name" {
   description = "Name of the Data Prepping Lambda"
   type        = string
+  default     = "data-prepping-lambda"
 }
 
 variable "plugin_logic_lambda_name" {
   description = "Name of the Plugin Logic Lambda"
   type        = string
+  default     = "pluggin-logic-lambda"
 }
 
 variable "opensearch_backend_lambda_name" {
   description = "Name of the OpenSearch Backend Lambda"
   type        = string
+  default     = "opensearch-backend-lambda"
 }
 
 variable "lambda_runtime" {
@@ -71,6 +78,7 @@ variable "lambda_runtime" {
 variable "api_gateway_name" {
   description = "Name of the API Gateway"
   type        = string
+  default     = "chatgbt-api"
 }
 
 variable "api_gateway_description" {
@@ -82,6 +90,7 @@ variable "api_gateway_description" {
 variable "event_rule_name" {
   description = "Name of the EventBridge Rule"
   type        = string
+  default     = "lambda-eventbridge"
 }
 
 variable "event_rule_description" {
@@ -98,6 +107,7 @@ variable "lambda_event_target_id" {
 variable "opensearch_domain_name" {
   description = "Domain name for OpenSearch backend"
   type        = string
+  default     = "opensearch-domain"
 }
 
 variable "opensearch_instance_type" {
@@ -122,17 +132,20 @@ variable "opensearch_volume_type" {
 variable "iam_role_name" {
   description = "Name of the IAM Role for Lambda execution"
   type        = string
+  default     = "lambda-iam"
 }
 
 variable "iam_policy_name" {
   description = "Name of the IAM Policy for Lambda execution"
   type        = string
+  default     = "lambda-iam-policy"
 }
 
 # CloudWatch Variables
 variable "cloudwatch_log_group_name" {
   description = "Name of the CloudWatch Log Group"
   type        = string
+  default     = "test-rule"
 }
 
 variable "cloudwatch_log_retention_days" {
